0ffaec41bbf6c11e5ddf93bce9a5941784331519
stephanie di dino <stephanie.didino@gmail.com>
2023-11-07 15:48:47 +0900

    Challenge complete (without the options).

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # R Read all
+  get 'tasks', to: 'tasks#index'
+
+  # C Create
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+
+  # R Read one
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # U Update
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # D Delete
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  # resources :tasks
 end
